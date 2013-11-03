83b0d4dd97cc59b38a07d628544b6007b34c3942
kubov <kubov2@gmail.com>
2013-11-03 13:19:52 +0100

    zhifengsoft fix, PIN PB0 and PB1 from original usbasp are now routed to PD2 and PD3 (check your zhifengsoft programmer if it's the same). Also fixed PROGMEM consts requirement in newer avr-gcc.

--- firmware/main.c
+++ firmware/main.c
@@ -306,8 +306,7 @@ int main(void) {
 	/* no pullups on USB and ISP pins */
 	PORTD = 0;
 	PORTB = 0;
-	/* all outputs except PD2 = INT0 */
-	DDRD = ~(1 << 2);
+	DDRD = 0xf3;
 
 	/* output SE0 for USB reset */
 	DDRB = ~0;
